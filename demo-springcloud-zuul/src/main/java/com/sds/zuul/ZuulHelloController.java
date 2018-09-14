e2ab425079b881142533a67a98a4bbc647f361fd
谭畅 <tanchang01@sunlands.com>
2018-09-14 18:16:11 +0800

    zuul 支持 retry 不会出现 服务掉线后的访问异常

--- demo-springcloud-zuul/src/main/java/com/sds/zuul/ZuulHelloController.java
+++ demo-springcloud-zuul/src/main/java/com/sds/zuul/ZuulHelloController.java
@@ -2,7 +2,6 @@ package com.sds.zuul;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,12 +18,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class ZuulHelloController {
 
     private static final Logger logger = LoggerFactory.getLogger(ZuulHelloController.class);
-    @Value("${expressList}")
-    private String express;
 
     @RequestMapping("/zuul/hello")
     public String hello(){
-        logger.info("这是个日志：{}",express);
+        logger.info("这是个日志：{}","zuul hello");
         return "zuul hello";
     }
 }
